8ab60f688aef786302f8f6117256454bf75e47ba
Borkata <borispenev89@gmail.com>
2017-04-14 16:50:30 +0200

    Made changes in App_Start > IdentityConfig.cs to ApplicationUserManager.PasswordValidator properties

--- BudgetManager/BudgetManager.Web/App_Start/IdentityConfig.cs
+++ BudgetManager/BudgetManager.Web/App_Start/IdentityConfig.cs
@@ -55,10 +55,10 @@ namespace BudgetManager.Web
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                //RequireNonLetterOrDigit = true,
+                //RequireDigit = true,
+                //RequireLowercase = true,
+                //RequireUppercase = true,
             };
 
             // Configure user lockout defaults
